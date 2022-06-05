3c520a1397a3ca794478adb3150b88081350e373
liangzifei <zifeiliang@hotmail.com>
2022-06-05 09:41:53 -0400

    Update generate_test.m

--- Matlab-CODE/generate_test.m
+++ Matlab-CODE/generate_test.m
@@ -1,15 +1,20 @@
+% generate test diffusin MRI data for TOD prediction uisng trained neural network
+clc;clear;
+% because of the memory limitation, we segment the data into several slabs to run.
+% Users can update the code to remove the slab segmentation. 
+repeat = 1; %repeat is the slab number
+sample_img = 1;
 function flag = generate_test(repeat, sample_img)
 %%
 %loop several times for the data generate.
 %%
-files = dir('R:\zhangj18lab\zhangj18labspace\Zifei_Data\MouseHuman_proj\Kim\tJN*');
+files = dir('.\Matlab-CODE\Kim\tJN*');
 % Get a logical vector that tells which is a directory.
 % dirFlags = [files.isdir];
 % Extract only those that are directories.
 folder_list = files;
-% folder_list = dir(['R:\zhangj18lab\zhangj18labspace\Zifei_Data\MouseHuman_proj\Kaffman_Exp55\1*']);
-folder_dwi =['R:\zhangj18lab\zhangj18labspace\Zifei_Data\MouseHuman_proj\Kim\'];
-% folder_tod = ['R:\zhangj18lab\zhangj18labspace\Zifei_Data\MouseHuman_proj\Kaffman_Exp55\Z_todVersions\TOD_fromTCK_lmax6\'];
+folder_dwi =['.\Matlab-CODE\Kim\'];
+% folder_tod = ['.\TOD_fromTCK_lmax6\'];
 %% start loop %%%%%%%%%%%%%%%%
 halfsize_input = 1;
 stride = 1;
@@ -17,13 +22,9 @@ stride = 1;
 slice0 =(repeat-1)*60+41;     count=0;
 %%
 dwi5000 = read_mrtrix([folder_dwi,folder_list(sample_img).name,'\rawdata1.mif'])
-%     fod_img = read_mrtrix([folder_dwi,'fod-z',file_list(sample_img).name(1:2),'.mif']);
-%     tod_img = read_mrtrix([folder_dwi,'tod_sample150normed',file_list(sample_img).name(1:2),'sh.mif']);
-% tod_img = read_mrtrix([folder_tod,'tod_fromtckTODp60_lmax6_to',folder_list(sample_img).name,'.mif']);
 %% dwi data preprocess S/S0 DATA
 dwi_data = double(dwi5000.data(:,:,:,1:62)); %tod_data = double(tod_img.data(:,:,:,1:28)); 
 clear dwi500;
-
 % fa_img = load_untouch_nii([folder_dwi,folder_list(sample_img).name,'\FA.nii']);
 mask_img = read_mrtrix([folder_dwi,folder_list(sample_img).name,'\mask.mif']);
 norm = mean(dwi_data(:,:,:,1:2),4);
@@ -34,12 +35,6 @@ dwi_data1 = dwi_data(:,:,:,3:end);
 dwi_data = dwi_data1./norm;
 
 clear dwi_data1;
-% tod_data(isnan(tod_data))=0;
-%% end normalize.
-% sum_tod4 = sum(tod_data,4); %sum_tod4(sum_tod4<10) = 0;
-% mask_data = logical(sum_tod4); fa_img.img(isnan(fa_img.img))=0;
-% mask_fa = fa_img.img; %mask_fa(mask_fa<0.01) = 0;
-% mask_fa = logical(mask_fa);
 
 mask_data = logical(mask_img.data); mask_data(isnan(mask_data))=0;
 %%
@@ -65,9 +60,9 @@ for slice=slice0:slice0+60%C-1%1+halfsize_input : stride : C-halfsize_input
 end
 if (exist('data')==1)
     data= permute(data,[5,1,2,3,4]);
-    writeNPY(data,'R:\zhangj18lab\zhangj18labspace\Zifei_Data\MouseHuman_proj\DeepNet_Learn\test_input.npy');
+    writeNPY(data,'.\Matlab-CODE\test_input.npy');
 %     label = permute(label,[5,1,2,3,4]);
-%     writeNPY(label,'R:\zhangj18lab\zhangj18labspace\Zifei_Data\MouseHuman_proj\DeepNet_Learn\test_output.npy');
+%     writeNPY(label,'.\Matlab-CODE\test_output.npy');
     flag = 1
 else
     flag =0
