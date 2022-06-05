3a9d636016a7aaeb5d2e69355797c321ada4c3ae
liangzifei <zifeiliang@hotmail.com>
2022-06-05 09:31:53 -0400

    Update generate_train_MATLABdwiTODKimKeras0427.m

--- Matlab-CODE/generate_train_MATLABdwiTODKimKeras0427.m
+++ Matlab-CODE/generate_train_MATLABdwiTODKimKeras0427.m
@@ -1,24 +1,21 @@
 clear;close all;
-files = dir('R:\zhangj18lab\zhangj18labspace\Zifei_Data\MouseHuman_proj\Kim');
+files = dir('.\Matlab-CODE\Kim');
 % Get a logical vector that tells which is a directory.
 dirFlags = [files.isdir];
 % Extract only those that are directories.
 folder_list = files(dirFlags); folder_list(1:2) = []; 
-% folder_list = dir(['R:\zhangj18lab\zhangj18labspace\Zifei_Data\MouseHuman_proj\Kaffman_Exp55\1*']);
-folder_dwi =['R:\zhangj18lab\zhangj18labspace\Zifei_Data\MouseHuman_proj\Kim\'];
-folder_tod = ['R:\zhangj18lab\zhangj18labspace\Zifei_Data\MouseHuman_proj\Kim\Z_tod\'];
+folder_dwi =['.\Matlab-CODE\Kim\'];
+folder_tod = ['.\Matlab-CODE\Kim\Z_tod\'];
 %% start loop %%%%%%%%%%%%%%%%
 halfsize_input = 1;
 stride = 3;
 count=0;
 % sample_num=[1,2,4,5];
 for sample_img = 1:7% length(folder_list)
-    %     dwi2000 = load_untouch_nii([folder_dwi,num2str(sample_img),'\rigidaffine_Lddm_dwi2000.img']);
+% read the training data
     dwi5000 = read_mrtrix([folder_dwi,folder_list(sample_img).name,'\rawdata1.mif'])
-    %     fod_img = read_mrtrix([folder_dwi,'fod-z',file_list(sample_img).name(1:2),'.mif']);
-    %     tod_img = read_mrtrix([folder_dwi,'tod_sample150normed',file_list(sample_img).name(1:2),'sh.mif']);
     tod_img = read_mrtrix([folder_tod,'\tod_fromtckTODp60_lmax6_to',folder_list(sample_img).name,'.mif']);
-    %% dwi data preprocess S/S0 DATA
+    %% dwi data preprocess S/S0 DATA normalize.
     dwi_data = double(dwi5000.data(:,:,:,1:62)); tod_data = double(tod_img.data(:,:,:,1:28)); clear dwi5000 tod_img;
     
     fa_img = read_mrtrix([folder_dwi,folder_list(sample_img).name,'\warped_P60.mif']);
@@ -34,15 +31,7 @@ for sample_img = 1:7% length(folder_list)
     
     clear dwi_data1;
     tod_data(isnan(tod_data))=0;
-    %     norm_todmat = repmat(norm_todmat,[1,1,1,45]);
-    %     tod_data = tod_data./norm_todmat;
-    %% normalize data form original.
-    %     S0 = dwi_data(:,:,:,1:2); S=dwi_data(:,:,:,3:end);
-    %     S0 = repmat(mean(S0,4),[1,1,1,size(S,4)]);
-    %     SdivS0 = S./S0; norms = sqrt(sum(SdivS0.^2,4));
-    %     norms = repmat(norms,[1,1,1,30]);
-    %     SdivS0= SdivS0./norms;
-    %     SdivS0(isnan(SdivS0))=0;
+
     %% end normalize.
     sum_tod4 = sum(tod_data,4); %sum_tod4(sum_tod4<10) = 0;
     mask_data = logical(sum_tod4).*logical(mask_img.data); fa_img.data(isnan(fa_img.data))=0;
@@ -82,9 +71,8 @@ order = randperm(count);
 data = data(:, :, :,:,order);
 label = label(:, :,:, :, order);
 %%
-%%
+% save .npy file for pytocrh training
 data = permute(data,[5,1,2,3,4]);
 writeNPY(data,'input.npy');
 label = permute(label,[5,1,2,3,4]);% label = label;
 writeNPY(label,'output.npy');
-% save traindataJG_allMRIsTOD.mat data label -v7.3;
